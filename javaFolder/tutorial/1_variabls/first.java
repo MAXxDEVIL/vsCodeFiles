49715eeac1dd6bf73dc2736703a0767be8aa3301
Mr_cat <paramitagopalpaul06@gmail.com>
2026-07-25 21:24:02 +0530

    Add first and main Java classes with basic functionality

---
+++ javaFolder/tutorial/1_variabls/first.java
@@ -0,0 +1,16 @@
+public class first {
+    public static void main(){
+
+        boolean isStudent = false;
+
+        if(isStudent){
+
+            System.out.println("You are a student ");
+        }
+        else{
+            System.out.println("You are not a student");
+        }
+
+
+    }
+}
---
+++ javaFolder/tutorial/2_Strings/main.java
@@ -0,0 +1,33 @@
+public class main {
+    
+    public static void main(){
+
+
+        // String :  is a datatype where we store variables
+        // It is a reffrence data type
+
+
+
+        String name = "Gopal Paul";
+        String food = "Beriyani";
+        String email = "zyx00@gmail.com";
+        String bike = "Rocket 3";
+
+
+
+        System.out.println("Hello " + name);
+        System.out.println("You love " + food);
+        System.out.println("Your email is " + email);
+        System.out.println("Your bike is " + bike);
+        
+
+
+
+
+        System.out.println("Your chois a ");
+
+
+    }
+
+
+}
