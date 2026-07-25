34d8a196bd4386229242b8ffcc8db7cfffb6cf15
Mr_cat <paramitagopalpaul06@gmail.com>
2026-07-25 21:55:54 +0530

    Add variables documentation and update main class with additional string variables

---
+++ javaFolder/tutorial/1_variabls/note.md
@@ -0,0 +1,26 @@
+## Variables
+
+> Variable = A reusable containor foe a value.
+             A variable behaves as if it was the value it contains.
+
+## Type of Variabls
+
+🍎 > Primitive = Simple value stored directly in memory (like "stack").
+
+🍏 > Nonprimitive = It stored memory address of auther variables.
+
+     🍎 Primitive    vs   🍏 Nonprimitive
+    --------------------------------------
+    Int(int)          |    String (String)
+                      |
+    Double (double)   |    Array (array)
+                      |
+    Char (char)       |    Object (object)
+                      |
+    Boolean (boolean) |
+                      |
+    Float (float)     |
+                      |
+
+
+
--- javaFolder/tutorial/2_Strings/main.java
+++ javaFolder/tutorial/2_Strings/main.java
@@ -12,19 +12,21 @@ public class main {
         String food = "Beriyani";
         String email = "zyx00@gmail.com";
         String bike = "Rocket 3";
+        String color = "red";
+        String year = "2025";
 
 
 
-        System.out.println("Hello " + name);
-        System.out.println("You love " + food);
-        System.out.println("Your email is " + email);
-        System.out.println("Your bike is " + bike);
+     // System.out.println("Hello " + name);
+     // System.out.println("You love " + food);
+     // System.out.println("Your email is " + email);
+     // System.out.println("Your bike is " + bike);
         
 
 
 
 
-        System.out.println("Your chois a ");
+     // System.out.println("Your chois a " + color + " " + year + " " + bike );
 
 
     }
