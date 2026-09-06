03a129af15d2ed3fcf1026461d5cb467780bab92
Gopal paul <paramitagopalpaul06@gmail.com>
2026-09-06 13:09:34 +0530

    Add Main class with initial setup for math functions

---
+++ javaFolder/tutorial/6_math_funtions/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        // Math funtions
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n1;
+        double n2;
+
+        
+
+
+
+    }
+
+
+}
