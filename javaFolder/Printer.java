287ea9b5abb0af73eeb33dcf7ed6a827aab1a7cc
Gopal paul <paramitagopalpaul06@gmail.com>
2026-09-06 12:15:03 +0530

    Implement if-else structure and add Printer class for greeting message

---
+++ javaFolder/Printer.java
@@ -0,0 +1,10 @@
+class Printer {
+    void display() {
+        System.out.println("Happy Teachers' Day ");
+    }
+
+    public static void main(String[] args) {
+        Printer obj = new Printer();
+        obj.display();
+    }
+}
--- javaFolder/tutorial/2_if_else/_if_else.java
+++ javaFolder/tutorial/2_if_else/_if_else.java
@@ -2,18 +2,9 @@ public class _if_else {
 
     public static void main(){
 
-        boolean isPass = true;
+        boolean isPass = false;
 
-        // If / Else (if, else)
-         // It use for chack a candition 
-
-        if (isPass) {
-            System.out.println("you are passed the exam");
-        }
-
-        else{
-            System.out.println("you did't pass the exam");
-        }
+      
 
 
     }
